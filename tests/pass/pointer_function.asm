4377e2488e4694d3eae82803556fd7b7ec8cd468
Wolfgang (Blub) Bumiller <blub@speed.at>
2009-08-15 12:16:37 +0200

    pointer_function testsuite target

---
+++ tests/pass/pointer_function.asm
@@ -0,0 +1,27 @@
+entity self;
+.float value;
+.float ptr;
+void (vector, vector, POINTER TYPE) vecadd;
+void(vector a, vector b, POINTER TYPE c) vecadd = asm
+{
+local vector temp_0;
+local vector temp_1;
+	ADD_I		c_x,	offset_0,	temp_1_x;
+	ADD_V		a,	b,	temp_0;
+	STOREP_V	temp_0_x,	temp_1;
+	DONE;
+}
+
+void (vector, vector, POINTER TYPE) vecadd2;
+void(vector a, vector b, POINTER TYPE c) vecadd2 = asm
+{
+local float temp_0;
+local vector temp_1;
+local vector temp_2;
+	C_FTOI	0.000000,	temp_0;
+	ADD_I		c_x,	temp_0,	temp_1_x;
+	ADD_V		a,	b,	temp_2;
+	STOREP_V	temp_2_x,	temp_1;
+	DONE;
+}
+
--- tests/pass/ternary_term.asm
+++ tests/pass/ternary_term.asm
@@ -5,8 +5,7 @@ float() test = asm
 {
 local float temp_0;
 	CALL1H	f,	3.000000;
-	NOT_F		return_x,	temp_0;
-	IF		temp_0,	3;
+	IFNOT		return_x,	3;
 	STORE_F	4.000000,	temp_0;
 	RETURN	temp_0;
 	STORE_F	5.000000,	temp_0;
