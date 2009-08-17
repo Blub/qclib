3d08a092a1168f9ac1ff924b048f8b643b993334
Wolfgang (Blub) Bumiller <blub@speed.at>
2009-08-17 18:40:11 +0200

    updated pass/pointer*

--- tests/pass/pointer_function.asm
+++ tests/pass/pointer_function.asm
@@ -6,7 +6,7 @@ void(vector a, vector b, POINTER TYPE c) vecadd = asm
 {
 local vector temp_0;
 local vector temp_1;
-	ADD_I		c_x,	offset_0,	temp_1_x;
+	ADD_I		c,	offset_0,	temp_1_x;
 	ADD_V		a,	b,	temp_0;
 	STOREP_V	temp_0_x,	temp_1;
 	DONE;
@@ -19,7 +19,8 @@ local float temp_0;
 local vector temp_1;
 local vector temp_2;
 	C_FTOI	0.000000,	temp_0;
-	ADD_I		c_x,	temp_0,	temp_1_x;
+	MUL_I		temp_0,	12,	temp_0;
+	ADD_I		c,	temp_0,	temp_1_x;
 	ADD_V		a,	b,	temp_2;
 	STOREP_V	temp_2_x,	temp_1;
 	DONE;
---
+++ tests/pass/pointer_to_pointer.asm
@@ -0,0 +1,67 @@
+entity self;
+.float value;
+POINTER TYPE p;
+POINTER TYPE v;
+void () assign;
+void() assign = asm
+{
+local float temp_0;
+	ADDRESS	self,	value,	temp_0;
+	STORE_P	temp_0,	v;
+	GET_POINTER	v,	offset_0,	temp_0;
+	STORE_P	temp_0,	p;
+	DONE;
+}
+
+floatptr () deref_once;
+floatptr() deref_once = asm
+{
+local float temp_0;
+	LOADP_I	p,	offset_0,	temp_0;
+	RETURN	temp_0;
+}
+
+float () deref_twice;
+float() deref_twice = asm
+{
+local float temp_0;
+	LOADP_I	p,	offset_0,	temp_0;
+	LOADP_F	temp_0,	offset_0,	temp_0;
+	RETURN	temp_0;
+}
+
+float () deref_index;
+float() deref_index = asm
+{
+local float temp_0;
+	C_FTOI	1.000000,	temp_0;
+	MUL_I		temp_0,	4,	temp_0;
+	LOADP_I	p,	temp_0,	temp_0;
+	LOADP_F	temp_0,	offset_0,	temp_0;
+	RETURN	temp_0;
+}
+
+float () index_twice;
+float() index_twice = asm
+{
+local float temp_0;
+local float temp_1;
+	C_FTOI	1.000000,	temp_1;
+	MUL_I		temp_1,	4,	temp_1;
+	LOADP_I	p,	temp_1,	temp_1;
+	C_FTOI	1.000000,	temp_0;
+	MUL_I		temp_0,	4,	temp_0;
+	LOADP_F	temp_1,	temp_0,	temp_0;
+	RETURN	temp_0;
+}
+
+float () evil_stuff;
+float() evil_stuff = asm
+{
+local float temp_0;
+	C_FTOI	1.000000,	temp_0;
+	MUL_I		temp_0,	4,	temp_0;
+	LOADP_F	v,	temp_0,	temp_0;
+	RETURN	temp_0;
+}
+
