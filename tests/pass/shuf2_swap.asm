1490bb054cec83832defbe9538b85a07776efa1d
Wolfgang (Blub) Bumiller <blub@speed.at>
2009-08-22 12:03:36 +0200

    the swap test missed a testcase, added now

--- tests/pass/shuf2_swap.asm
+++ tests/pass/shuf2_swap.asm
@@ -4,6 +4,10 @@ float() test = asm
 {
 local vector y;
 local vector x;
+local float temp_0;
+	STORE_F	x_x,	temp_0;
+	STORE_F	x_y,	x_x;
+	STORE_F	temp_0,	x_y;
 	STORE_F	y_y,	x_x;
 	STORE_F	y_x,	x_y;
 	RETURN	0.000000;
