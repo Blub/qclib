b62d0d30bf6b1609d456d2e9c7d949199796d8bc
Wolfgang (Blub) Bumiller <blub@speed.at>
2009-08-29 21:48:18 +0200

    adadpt testcases, -fiffloat isn't default anymore

--- tests/pass/if_i_without_op_support.asm
+++ tests/pass/if_i_without_op_support.asm
@@ -2,9 +2,7 @@ float () test;
 float() test = asm
 {
 local float x;
-local float temp_0;
-	NE_F		x,	0.000000,	temp_0;
-	IFNOT		temp_0,	2;
+	IFNOT		x,	2;
 	RETURN	0.000000;
 	RETURN	1.000000;
 }
--- tests/pass/if_i_without_op_support2.asm
+++ tests/pass/if_i_without_op_support2.asm
@@ -2,7 +2,9 @@ float () test;
 float() test = asm
 {
 local float x;
-	IFNOT		x,	2;
+local float temp_0;
+	NE_F		x,	0.000000,	temp_0;
+	IFNOT		temp_0,	2;
 	RETURN	0.000000;
 	RETURN	1.000000;
 }
